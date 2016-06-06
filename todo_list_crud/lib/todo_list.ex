f9adf9d42d88c266bb8e72bc6b6249b8031fade5
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2016-06-06 08:51:10 +0100

    Fixed issues with the addition of items to the list

--- todo_list_crud/lib/todo_list.ex
+++ todo_list_crud/lib/todo_list.ex
@@ -4,12 +4,12 @@ defmodule TodoList do
   def new, do: %TodoList{}#returns a new struct
 
   def add_entry(
-    %TodoList{ auto_id: auto_id, entries: entries } = todo_list,
-    %{date: date, title: title}= entry
+    %TodoList{ entries: entries, auto_id: auto_id } = todo_list,
+    entry
   ) do
     entry = Map.put(entry, :id, auto_id)#set the new id to the entry passed
 
-    new_entries = Map.put(todo_list, auto_id, entry)#adding new entry to list
+    new_entries = Map.put(entries, auto_id, entry)#adding new entry to list
     #reset the struct by updating the entries and the auto_id
     #Map update sintax is  %{map_to_update | field: value, field1: value1}
     #When using the update syntax (|), the VM is aware that no new keys will be added to the struct
--- todo_list_crud/test/todo_list_test.exs
+++ todo_list_crud/test/todo_list_test.exs
@@ -6,37 +6,25 @@ defmodule TodoListTest do
   end
 
   test "Can add an item to an empty todo list" do
-    expected =
-          %TodoList{
-            auto_id: 2,
-            entries: %{
-              1 => %{
-                date: {2016, 5, 15},
-                id: 1,
-                title: "First item"
-              },
-              :__struct__ => TodoList, :auto_id => 1, :entries => %{}
-            }
-          }
+    expected =%TodoList{
+      auto_id: 2,
+      entries: %{1 => %{date: {2016, 5, 15}, id: 1, title: "First item"}}
+    }
+
     actual = TodoList.new |>
       TodoList.add_entry(%{date: {2016, 05, 15}, title: "First item"})
     assert actual == expected
   end
 
   test "Can add an item to a non empty list" do
-    expected =
-          %TodoList{
-            auto_id: 2,
-            entries: %{
-              1 => %{
-                date: {2016, 5, 15},
-                id: 1,
-                title: "First item"
-              },
-              :__struct__ => TodoList, :auto_id => 1, :entries => %{}
-            }
-          }
-          
+    expected = %TodoList{
+      auto_id: 3,
+      entries: %{
+        1 => %{date: {2016, 5, 15}, id: 1, title: "First item"},
+        2 => %{date: {2016, 5, 16}, id: 2, title: "Second item"}
+      }
+    }
+
     actual = TodoList.new |>
       TodoList.add_entry(%{date: {2016, 05, 15}, title: "First item"}) |>
       TodoList.add_entry(%{date: {2016, 05, 16}, title: "Second item"})
