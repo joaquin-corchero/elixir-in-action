a5c41ea3f4de2c13d62ff883ae04bb33525b808e
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2016-06-13 11:05:03 +0100

    created the file line reader and test

--- todo_list_crud/lib/todo_list_csv_importer.ex
+++ todo_list_crud/lib/todo_list_csv_importer.ex
@@ -1,11 +1,35 @@
 defmodule TodoList.CsvImporter do
   def import(file_location) do
     file_location
+    |> TodoList.FileReader.get_lines
     |> get_entries
     |> TodoList.new
   end
 
-  defp get_entries(file_location) do
-    #2655
+  defp get_entries(lines) do
+    lines
+    |> Enum.reduce([],
+      fn(line, entries) ->
+        raw = String.split(line, ",")
+        date = get_date(String.split(raw[0], "/"))
+        entries.push(%{date: date, title: raw[1]})
+      end
+    )
+  end
+
+  defp get_date([day, month, year]) do
+    {Integer.parse(year), Integer.parse(month), Integer.parse(day) }
+  end
+end
+
+defmodule TodoList.FileLineReader do
+  import File
+
+  def get_lines(file_location) do
+    {_result, content} = File.read (file_location)
+
+    String.replace(content,"\r\n", "\n" )
+    |> String.strip
+    |> String.split("\n")
   end
 end
--- todo_list_crud/test/todo_list_csv_importer_test.exs
+++ todo_list_crud/test/todo_list_csv_importer_test.exs
@@ -14,3 +14,12 @@ defmodule TodoList.CsvImporterShould do
     assert actual == expected
   end
 end
+
+defmodule TodoList.FileLineReaderShould do
+  use ExUnit.Case
+
+  test "get the correct number lines on a file" do
+    actual = TodoList.FileLineReader.get_lines("test/data/todo_list.csv")
+    assert actual == ["2016/6/15,First item", "2016/5/16,Second item"]
+  end
+end
