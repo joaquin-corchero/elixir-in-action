b307d9061fc225d40b9323a3d0b3148f01e42942
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-06-13 23:34:40 +0100

    Deleted the old directory

--- todo_list_crud/lib/infrastructure/csv_reader.ex
+++
@@ -1,8 +0,0 @@
- defmodule Infrastructure.CsvReader do
-   def read(file_location) do
-     {_result, content} = File.read (file_location)
-     String.replace(content,"\r\n", "\n" )
-     |> String.strip
-     |> String.split("\n")
-   end
- end
