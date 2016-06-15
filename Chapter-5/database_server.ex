6acb0e50201de8aecf48b330ba3700d6d93d937e
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-06-15 22:01:28 +0100

    started implementation of the database server

--- Chapter-5/database_server.ex
+++ Chapter-5/database_server.ex
@@ -5,9 +5,19 @@ defmodule DatabaseServer do
 
   defp loop do
     receive do
-      {:msg,contents} ->
+      {:run_query, caller, query_def} ->
+        #runs the query and sends the results to the caller
+        send(caller, {:query_result, run_query(query_def_)})
     end
 
     loop
   end
+
+  #query execution
+  defp run_query(query_def) do
+    :timer.sleep(2000)
+    "#{{query_def}} result"
+  end
+
+  
 end
